917a6d6443e3b81763f5784a665dccb585eb5c13
Ancientshield <jacklin2100@gmail.com>
2023-02-26 22:52:53 +0800

    064-comparison-with-the-greater-than-or-equal-to-operator

---
+++ Basic/058-comparison-with-the-inequality-operator.js
@@ -0,0 +1,9 @@
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // 修改這一行
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
---
+++ Basic/059-comparison-with-the-strict-equality-operator.js
@@ -0,0 +1,11 @@
+// Setup
+function testStrict(val) {
+  if (val === 7) { // 修改這一行
+    return "Equal";
+  }
+  else {
+    return "Not Equal";
+  }
+}
+
+testStrict(10);
