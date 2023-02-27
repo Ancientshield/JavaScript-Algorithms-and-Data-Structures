5f88aa79983a730583d47b541b39a1c81f4fb5ad
Ancientshield <jacklin2100@gmail.com>
2023-02-27 23:52:05 +0800

    070-comparisons-with-the-logical-or-operator

---
+++ Basic/070-comparisons-with-the-logical-or-operator.js
@@ -0,0 +1,14 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val<10 || val>20) {
+    return "Outside";
+  }
+  // 只修改這一行上面的代碼
+  return "Inside";
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
