9f552cbe2567259e4ea496581683b34a1bbec912
Ancientshield <jacklin2100@gmail.com>
2023-03-02 18:59:00 +0800

    112-use-recursion-to-create-a-countdown

---
+++ Basic/105-nesting-for-loops.js
@@ -0,0 +1,9 @@
+function randomFraction() {
+
+  // Only change code below this line
+  let random = Math.random();
+  while (random !== 0){
+    return random;
+  }
+  // Only change code above this line
+}
