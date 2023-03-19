7ed3778803bab6fcd5e3b31c7aece9c8f44c8e03
Ancientshield <jacklin2100@gmail.com>
2023-03-19 15:48:22 +0800

    008-use-the-filter-method-to-extract-data-from-an-array_filter_map.js

---
+++ Functional Programming/007-implement-map-on-a-prototype_this_forEach_cbfk.js
@@ -0,0 +1,14 @@
+Array.prototype.myMap = function (callback) {
+	const newArray = [];
+	// Only change code below this line
+	// 這裡的 this 是指 function，也就是 myMap
+	this.forEach((currentValue, currentValueArrayIndex, currentValueArray) => {
+		return newArray.push(
+			callback(currentValue, currentValueArrayIndex, currentValueArray)
+		);
+	});
+	// forEach 後面兩個參數就是 Array 的 index 與對應 Array index 的 value
+	// 在每次 push 的時候，再呼叫 callback function
+	// Only change code above this line
+	return newArray;
+};
