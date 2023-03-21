9fd672b4e8159c637d1430fcb9770eb6e1a0a53b
Ancientshield <jacklin2100@gmail.com>
2023-03-21 21:07:28 +0800

    014-use-the-reduce-method-to-analyze-data_map_reduce_filter.js

---
+++ Functional Programming/011-remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -0,0 +1,9 @@
+function nonMutatingSplice(cities) {
+  // 只修改這一行下面的代碼
+  return cities.slice(0, 3);
+
+  // 只修改這一行上面的代碼
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
